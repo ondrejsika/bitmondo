a989c26a3ca89d01de3662848be029c1b067b883
Ondrej Sika <ondrej@ondrejsika.com>
2016-01-23 16:28:29 +0000

    libbitmondo - create simple bitcoin-style feed wraper

---
+++ libbitmondo.py
@@ -0,0 +1,34 @@
+BITCOIN_IMG_URL = 'https://bitstickers.net/wp-content/uploads/2013/12/btc-orange.jpg'
+
+def shorten_address(address, size=6):
+    return '%s...%s' % (address[:size], address[-size:])
+
+
+class BitMondoClient(object):
+    def __init__(self, mondo_session, account_id):
+        self._mondo_session = mondo_session
+        self._account_id = account_id
+
+    def incoming_payment(self, from_address, to_address, amount_btc, tx_id):
+        self._mondo_session.create_feed_item(
+            self._account_id,
+            'Bitcoin tx %s BTC' % amount_btc,
+            'from %s to %s' % (from_address, to_address),
+            BITCOIN_IMG_URL,
+            'https://blockchain.info/tx/%s' % tx_id,
+            '#008000',
+            '#000',
+            '#FFF',
+        )
+
+    def outgoing_payment(self, from_address, to_address, amount_btc, tx_id):
+        self._mondo_session.create_feed_item(
+            self._account_id,
+            'Bitcoin tx %s BTC' % amount_btc,
+            'from %s to %s' % (shorten_address(from_address), shorten_address(to_address)),
+            BITCOIN_IMG_URL,
+            'https://blockchain.info/tx/%s' % tx_id,
+            'FF0000',
+            '#000',
+            '#FFF',
+            )
--- mondo/views.py
+++ mondo/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.conf import settings
 
 from libmondo import MondoOauthClient, MondoSessionClient
+from libbitmondo import BitMondoClient
 
 
 mondo_client = MondoOauthClient(
@@ -29,17 +30,16 @@ def authorize_view(request):
     account_id = mondo_client.get_accounts(token)['accounts'][0]['id']
     print account_id
     print token
-    print mondo_session.create_feed_item(
-        account_id,
-        'hello',
-        'from London',
-        'https://bitstickers.net/wp-content/uploads/2013/12/btc-orange.jpg',
-        'https://blockchain.info/address/1K8jWKBgWU2L1zvBbTn4G3vMyJx8Ra1J6G',
-        '#000',
-        '#000',
-        '#FFF',
-    )
-
+    bitmondo = BitMondoClient(mondo_session, account_id)
+    bitmondo.incoming_payment('1Bdii7sGaE2i6AFUPThTCoFsXWKJdSJ955',
+                              '1MNe417Sx3WGtbdcZg2v8wnBPodkbsL1R8',
+                              6.543,
+                              '16aef80dd4cfd5fe717f19151f9bd9491b13c30171bacdd96a6ab6366459fbe')
+    bitmondo.outgoing_payment('1Bdii7sGaE2i6AFUPThTCoFsXWKJdSJ955',
+                              '1MNe417Sx3WGtbdcZg2v8wnBPodkbsL1R8',
+                              6.543,
+                              '16aef80dd4cfd5fe717f19151f9bd9491b13c30171bacdd96a6ab6366459fbe')
+    a
     return HttpResponseRedirect('/mondo')
 
 

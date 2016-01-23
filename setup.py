65af3652da45cd78486d30a18420263425aab7cd
Ondrej Sika <ondrej@ondrejsika.com>
2016-01-23 15:58:11 +0000

    setup - add simple package conf.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='bitmondo',
+    version='0.0.0',
+    description='Show your Bitcoin transactions at the Mondo feed',
+    url='https://github.com/ondrejsika/bitmondo',
+    author='Ondrej Sika',
+    author_email='ondrej@ondrejsika.com',
+    packages=find_packages(),
+    install_requires=[
+        'django==1.8.8',
+        'requests==2.9.1',
+    ],
+)
+
